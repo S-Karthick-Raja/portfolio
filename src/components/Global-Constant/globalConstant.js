9b57dce8aa2f565c0e67d0a57a27260eab1a5b41
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2022-02-15 02:45:49 +0530

    mockapi added

---
+++ src/components/Global-Constant/globalConstant.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://620ab63992946600171c5c3e.mockapi.io";
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,33 +1,75 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import { Button } from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import FoodBankIcon from '@mui/icons-material/FoodBank';
-import LanguageIcon from '@mui/icons-material/Language';
+import React, { useEffect } from "react";
+import { Button } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import LanguageIcon from "@mui/icons-material/Language";
+import { API_URL } from './Global-Constant/globalConstant';
+import { useState } from 'react';
 
 function Project() {
+    const [project, setProject] = useState([]);
+
+    const getProjects = () => {
+        fetch(`${API_URL}/Portfolio`)
+            .then((data) => data.json())
+            .then((pjt) => setProject(pjt));
+    };
+    useEffect(getProjects, []);
+
     return (
         <section className="services" id="Projects">
             <div className="max-width">
                 <h2 className="title">My Projects</h2>
                 <div className="serv-content">
-                    <div className="card">
-                        <div className="box">
-                            <div className="text"> <FoodBankIcon /> Recipe Application</div>
-                            <h3>ABOUT</h3>
-                            <p>Here You Can Create Your Individual Recipe. It can be added to our side.we can update it and if not we can also delete the recipes.</p>
-                            <p className='project-btn'>
-                                <Button variant="contained" color="success"><GitHubIcon className='icon' /> Source Code</Button>
-                            </p>
-                            <p className='live-btn'>
-                                <Button variant="contained" color="error"><LanguageIcon className='icon' /> <a className='icon' href="https://day-31-task.netlify.app/" target="_blank" rel="noreferrer">View Site</a> </Button>
-                            </p>
+                    {project.map(({ avatar, recipe, about, sourceUrl, deploy }) => (
+                        <div className="card">
+                            <div className="avatar">
+                                <img className="Project-image" src={avatar} alt="recipe-app" />
+                            </div>
+                            <div className="box">
+                                <div className="text">
+                                    {" "}
+                                    <span className="recipe-name">
+                                        <RestaurantMenuIcon /> {recipe}
+                                    </span>
+                                </div>
+                                <p>
+                                    {about}
+                                </p>
+                                <p className="project-btn">
+                                    <Button variant="contained" color="success">
+                                        <GitHubIcon className="icon" />
+                                        <a
+                                            className="icon"
+                                            href={sourceUrl}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Source Code
+                                        </a>{" "}
+                                    </Button>
+                                </p>
+                                <p className="live-btn">
+                                    <Button variant="contained" color="error">
+                                        <LanguageIcon className="icon" />{" "}
+                                        <a
+                                            className="icon"
+                                            href={deploy}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            View Site
+                                        </a>{" "}
+                                    </Button>
+                                </p>
+                            </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
-export default Project
+export default Project;
