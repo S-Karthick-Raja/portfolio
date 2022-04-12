09092465929d6abd99339d6f0fd4e573bb66836b
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2022-04-12 11:47:06 +0530

    media query for homesection

--- src/components/Home/Homesection.js
+++ src/components/Home/Homesection.js
@@ -24,7 +24,7 @@ function Homesection() {
               }}
             />
           </div>
-          <a href="#contact">Hire me</a>
+          <a href="#contact" >Hire me</a>
         </div>
       </div>
     </section>
