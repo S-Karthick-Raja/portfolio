d4a1331521de6a6d3256abc6d723870d09c177e5
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2022-04-12 11:58:54 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 // import Topnavbar from './components/Topnavbar';
 import Homesection from './components/Home/Homesection';
-import Aboutsection from './components/Aboutsection';
-import Skillsection from './components/Skillsection';
-import Contactsection from './components/Contactsection';
+// import Aboutsection from './components/Aboutsection';
+// import Skillsection from './components/Skillsection';
+// import Contactsection from './components/Contactsection';
 import Footer from './components/Footer';
 import Topnavbar from './components/Topbar/Topnavbar';
-import ProjectDetail from './components/ProjectDetail';
+// import ProjectDetail from './components/ProjectDetail';
 
 
 function App() {
